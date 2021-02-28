311c111b526499833696ba33766b8b3cc2dfe944
Kyle Lemire <hello@klemire.com>
2021-02-27 20:01:45 -0600

    feat: set up styled components. built a few header components to test. set up custom font family

---
+++ components/HomeLayout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from './Layout';
+import { Wrapper } from './styled/global';
+
+const HomeLayout = () => {
+  return (
+    <Layout>
+      <Wrapper></Wrapper>
+    </Layout>
+  );
+};
+
+export default HomeLayout;
