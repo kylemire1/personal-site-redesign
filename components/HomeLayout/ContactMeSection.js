c84ad236453c46543705cbd664a6894477c98de6
Kyle Lemire <hello@klemire.com>
2021-02-28 23:31:09 -0600

    feat: finished first pass of home page gero section

--- components/HomeLayout/ContactMeSection.js
+++ components/HomeLayout/ContactMeSection.js
@@ -26,6 +26,7 @@ const ContactMeSection = () => {
 };
 
 const ContactMeNow = styled(PageSection)`
+  position: relative;
   grid-column: 2 / -1;
   background-color: ${vars.colorPrimaryDark};
   border-top-left-radius: ${vars.borderRadiusLarge};
@@ -33,11 +34,23 @@ const ContactMeNow = styled(PageSection)`
 
 const ContactHeading = styled(Heading)`
   color: ${vars.colorPrimaryLight};
-  font-size: ${vars.fontSizeHeading2};
+  font-size: ${vars.fontSizeHeading1};
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+
+  @media (min-height: ${vars.breakpointMedium}) {
+    font-size: ${vars.fontSizeHeading2};
+    font-weight: black;
+  }
 `;
 
 const ContactText = styled.p`
   color: ${vars.colorPrimaryLight};
+  font-size: ${vars.fontSizeTextSmall};
+
+  @media (min-height: ${vars.breakpointMedium}) {
+    font-size: ${vars.fontSizeText};
+  }
 `;
 
 const ButtonWrapper = styled.div`
