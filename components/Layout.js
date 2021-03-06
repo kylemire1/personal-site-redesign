32da266d4f47b1a4724520f127103927de46769b
Kyle Lemire <hello@klemire.com>
2021-03-05 19:12:30 -0600

    fix: fixed FOUC for real this time

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useRouter } from 'next/router';
 
 const Layout = ({ children }) => {
+  const wrapperRef = useRef();
   const { pathname } = useRouter();
 
-  return <div className={pathname === '/' ? 'section' : ''}>{children}</div>;
+  useEffect(() => {
+    document.addEventListener('DOMContentLoaded', documentLoadedCallback);
+
+    return () =>
+      document.removeEventListener('DOMContentLoaded', documentLoadedCallback);
+  }, []);
+
+  const documentLoadedCallback = () => {
+    if (typeof wrapperRef.current !== 'undefined') {
+      wrapperRef.current.style.visibility = 'visible';
+    }
+  };
+
+  return (
+    <div
+      ref={wrapperRef}
+      style={{ visibility: 'hidden' }}
+      className={pathname === '/' ? 'section' : ''}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Layout;
