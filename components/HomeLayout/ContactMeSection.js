4a3aaed819758c7bb61b3d0443d42cbf9bbf0cdd
Kyle Lemire <hello@klemire.com>
2021-03-05 19:18:09 -0600

    cleanup

--- components/HomeLayout/ContactMeSection.js
+++ components/HomeLayout/ContactMeSection.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 
 import MessageButton from '../MessageButton';
--- components/icons/Pdf.js
+++ components/icons/Pdf.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 const Pdf = () => {
   return (
     <svg
