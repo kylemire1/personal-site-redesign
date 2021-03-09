c51cdc2717b294d68541fb10cc0164408d9d422d
Kyle Lemire <hello@klemire.com>
2021-03-09 15:48:15 -0600

    fix: fixing next config to support image optimization

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ const withImages = require('next-images');
 
 module.exports = withImages({
   target: 'serverless',
+  images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/klemire/image/upload/',
+  },
 });
