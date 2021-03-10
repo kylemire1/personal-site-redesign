737b2777e7128721098464c21232d61f6f5ee48f
Kyle Lemire <hello@klemire.com>
2021-03-09 20:56:55 -0600

    chore: deleted old version of the site

--- gatsby-port/src/components/ExperienceLayout/lookingForData.js
+++
@@ -1,9 +0,0 @@
-const lookingForData = [
-  'Web Design',
-  'Front End Development',
-  'GatsbyJS Consulting',
-  'Contract Work',
-  'Web Accessibility Consulting',
-];
-
-export default lookingForData;
