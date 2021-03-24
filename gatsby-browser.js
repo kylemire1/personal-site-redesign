acafc908c9519831db077922fd39406fa5cf37eb
Kyle Lemire <hello@klemire.com>
2021-03-24 15:38:54 -0500

    refactored global styles

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,68 +1,19 @@
 import React from 'react';
-import { createGlobalStyle } from 'styled-components';
 
 import { ResumeContextProvider } from './contexts/ResumeContext';
 import { LayoutContextProvider } from './contexts/LayoutContext';
-
-import vars from './src/styles/vars';
+import GlobalStyles from './src/styles/GlobalStyles';
 
 import './src/styles/normalize.css';
-import './src/styles/globals.scss';
+import './src/styles/global-reset.scss';
 
 export const wrapRootElement = ({ element }) => {
   return (
     <ResumeContextProvider>
       <LayoutContextProvider>
-        <GlobalStyle />
+        <GlobalStyles />
         {element}
       </LayoutContextProvider>
     </ResumeContextProvider>
   );
 };
-
-const GlobalStyle = createGlobalStyle`
-  html {
-    overflow-x: hidden;
-    font-size: 72%;
-    background-color: ${vars.colorPrimary};
-
-    @media (min-width: ${vars.breakpointTiny}) {
-      font-size: 85%;
-    }
-
-    @media (min-width: ${vars.breakpointLarge}) {
-      font-size: 100%;
-    }
-  }
-
-  body {
-    overflow-x: hidden;
-  }
-
-  p {
-    font-weight: ${vars.fontWeightLight};
-    font-size: ${vars.fontSizeText};
-    line-height: 1.5;
-    max-width: 75ch;
-  }
-
-  p:not(:last-child) {
-    margin-bottom: 1rem;
-  }
-
-  ul {
-    padding-left: 1.25rem;
-    margin-bottom: 1.5rem;
-    li + li {
-      margin-top: 1em;
-    }
-  }
-
-  .section-wrapper + .section-wrapper {
-    padding-top: 8rem;
-
-    @media (min-width: ${vars.breakpointExtraSmall}) {
-      padding-top: 10rem;
-    }
-  }
-`;
