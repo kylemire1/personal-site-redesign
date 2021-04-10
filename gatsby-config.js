51fb4396ce6ccc35433302399617a627779bd547
Kyle Lemire <hello@klemire.com>
2021-04-10 14:56:57 -0500

    updated packagejson and site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chicago based web developer Kyle Lemire`,
+    description: `The personal portfolio site of Chicago web developer Kyle Lemire`,
+    author: `@kylelemire1`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
